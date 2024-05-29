8bf1b80dd068aa4b38d1a6d7e12f23b7d9bac006
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-05-29 16:34:47 +0900

    hoge

--- learn/src/jsMain/kotlin/App.kt
+++ learn/src/jsMain/kotlin/App.kt
@@ -5,8 +5,11 @@ import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.img
+import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.p
+import react.dom.html.ReactHTML.ul
 import web.cssom.ClassName
+import web.cssom.NamedColor
 import web.cssom.px
 
 val MyButton = FC {
@@ -47,6 +50,32 @@ val Profile = FC {
         css {
             width = (user["imageSize"] as Int).px
             height = (user["imageSize"] as Int).px
+        }
+    }
+}
+
+data class Product(
+    val title: String,
+    val isFruit: Boolean,
+    val id: Int,
+)
+
+val products = listOf(
+    Product(title = "Cabbage", isFruit = false, id = 1),
+    Product(title = "Garlic", isFruit = false, id = 2),
+    Product(title = "Apple", isFruit = true, id = 3),
+)
+
+val ShoppingList = FC {
+    ul {
+        products.forEach { product ->
+            li {
+                key = product.id.toString()
+                css {
+                    color = if (product.isFruit) NamedColor.magenta else NamedColor.darkgreen
+                }
+                +product.title
+            }
         }
     }
 }
