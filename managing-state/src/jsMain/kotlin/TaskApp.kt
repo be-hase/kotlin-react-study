d5891334f4ec2280a8daeb39e8ceb623897288a6
ryosuke-hasebe <ryosuke.hasebe@linecorp.com>
2024-05-31 17:41:17 +0900

    hoge

--- managing-state/src/jsMain/kotlin/TaskApp.kt
+++ managing-state/src/jsMain/kotlin/TaskApp.kt
@@ -1,130 +1,38 @@
+import react.Dispatch
 import react.FC
 import react.Props
+import react.PropsWithChildren
 import react.Reducer
+import react.createContext
 import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.h1
 import react.dom.html.ReactHTML.input
 import react.dom.html.ReactHTML.label
 import react.dom.html.ReactHTML.li
 import react.dom.html.ReactHTML.ul
+import react.useContext
 import react.useReducer
 import react.useState
 import web.html.InputType
 
-val TaskApp = FC {
+val TasksContext = createContext<List<Task>?>(null)
+
+val TasksDispatchContext = createContext<Dispatch<TaskAction>?>(null)
+
+fun useTasks() = checkNotNull(useContext(TasksContext))
+
+fun useTasksDispatch() = checkNotNull(useContext(TasksDispatchContext))
+
+val TasksProvider = FC<PropsWithChildren> { props ->
     val (tasks, dispatch) = useReducer(tasksReducer, initialTasks)
 
-    val handleAddTask: (String) -> Unit = { text ->
-        dispatch(TaskAction.Added(id = nextId++, text = text))
-    }
-
-    val handleChangedTask: (Task) -> Unit = { task ->
-        dispatch(TaskAction.Changed(task))
-    }
-
-    val handleDeleteTask: (Int) -> Unit = { taskId ->
-        dispatch(TaskAction.Deleted(taskId))
-    }
-
-    h1 { +"Prague itinerary" }
-    AddTask {
-        onAddTask = handleAddTask
-    }
-    TaskList {
-        this.tasks = tasks
-        onChangeTask = handleChangedTask
-        onDeleteTask = handleDeleteTask
-    }
-}
-
-external interface AddTaskProps : Props {
-    var onAddTask: (String) -> Unit
-}
-
-val AddTask = FC<AddTaskProps> { props ->
-    var text: String by useState("")
-
-    input {
-        placeholder = "Add task"
-        value = text
-        onChange = { text = it.target.value }
-    }
-    button {
-        onClick = {
-            text = ""
-            props.onAddTask(text)
-        }
-        +"Add"
-    }
-}
-
-external interface TaskListProps : Props {
-    var tasks: List<Task>
-    var onChangeTask: (Task) -> Unit
-    var onDeleteTask: (Int) -> Unit
-}
-
-val TaskList = FC<TaskListProps> { props ->
-    ul {
-        props.tasks.forEach { task ->
-            li {
-                key = task.id.toString()
-                TaskComponent {
-                    this.task = task
-                    onChange = props.onChangeTask
-                    onDelete = props.onDeleteTask
-                }
-            }
+    TasksContext.Provider {
+        value = tasks
+        TasksDispatchContext.Provider {
+            value = dispatch
+            +props.children
         }
     }
-}
-
-external interface TaskProps : Props {
-    var task: Task
-    var onChange: (Task) -> Unit
-    var onDelete: (Int) -> Unit
-}
-
-val TaskComponent = FC<TaskProps> { props ->
-    var isEditing: Boolean by useState(false)
-
-    label {
-        input {
-            type = InputType.checkbox
-            checked = props.task.done
-            onChange = {
-                props.onChange(props.task.copy(done = it.target.checked))
-            }
-        }
-        if (isEditing) {
-            input {
-                value = props.task.text
-                onChange = {
-                    props.onChange(props.task.copy(text = it.target.value))
-                }
-            }
-            button {
-                onClick = { isEditing = false }
-                +"Save"
-            }
-        } else {
-            +props.task.text
-            button {
-                onClick = { isEditing = true }
-                +"Edit"
-            }
-        }
-        button {
-            onClick = { props.onDelete(props.task.id) }
-            +"Delete"
-        }
-    }
-}
-
-sealed interface TaskAction {
-    data class Added(val id: Int, val text: String) : TaskAction
-    data class Changed(val task: Task) : TaskAction
-    data class Deleted(val id: Int) : TaskAction
 }
 
 val tasksReducer: Reducer<List<Task>, TaskAction> = { tasks, action ->
@@ -161,3 +69,91 @@ val initialTasks = listOf(
     Task(id = 1, text = "Watch a puppet show", done = false),
     Task(id = 2, text = "Lennon Wall pic", done = false),
 )
+
+val TaskApp = FC {
+    TasksProvider {
+        h1 { +"Prague itinerary" }
+        AddTask {}
+        TaskList {}
+    }
+}
+
+val AddTask = FC {
+    var text: String by useState("")
+    val dispatch = useTasksDispatch()
+
+    input {
+        placeholder = "Add task"
+        value = text
+        onChange = { text = it.target.value }
+    }
+    button {
+        onClick = {
+            text = ""
+            dispatch(TaskAction.Added(id = nextId++, text = text))
+        }
+        +"Add"
+    }
+}
+
+val TaskList = FC {
+    val tasks = useTasks()
+
+    ul {
+        tasks.forEach { task ->
+            li {
+                key = task.id.toString()
+                TaskComponent {
+                    this.task = task
+                }
+            }
+        }
+    }
+}
+
+external interface TaskComponentProps : Props {
+    var task: Task
+}
+
+val TaskComponent = FC<TaskComponentProps> { props ->
+    var isEditing: Boolean by useState(false)
+    val dispatch = useTasksDispatch()
+
+    label {
+        input {
+            type = InputType.checkbox
+            checked = props.task.done
+            onChange = {
+                dispatch(TaskAction.Changed(props.task.copy(done = it.target.checked)))
+            }
+        }
+        if (isEditing) {
+            input {
+                value = props.task.text
+                onChange = {
+                    dispatch(TaskAction.Changed(props.task.copy(text = it.target.value)))
+                }
+            }
+            button {
+                onClick = { isEditing = false }
+                +"Save"
+            }
+        } else {
+            +props.task.text
+            button {
+                onClick = { isEditing = true }
+                +"Edit"
+            }
+        }
+        button {
+            onClick = { dispatch(TaskAction.Deleted(props.task.id)) }
+            +"Delete"
+        }
+    }
+}
+
+sealed interface TaskAction {
+    data class Added(val id: Int, val text: String) : TaskAction
+    data class Changed(val task: Task) : TaskAction
+    data class Deleted(val id: Int) : TaskAction
+}
